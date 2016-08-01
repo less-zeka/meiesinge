8bccf0327ad352fa98b952c8c512f862e45ca649
Less <info@open-source.tips>
2016-08-01 17:09:38 +0200

    inviteecontroller

---
+++ WebClient/Controllers/InviteeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Meiesinge.WebClient.Controllers
+{
+    public class InviteeController : Controller
+    {
+        // GET: Invitee
+        [Route("Invitee")]
+        public ActionResult Index()
+        {
+            return Test(Guid.Empty);
+        }
+
+        [Route("Test/{id}")]
+        public ActionResult Test(Guid? id)
+        {
+            return View("Index",id.Value);
+        }
+    }
+}
---
+++ WebClient/Models/InvitationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Meiesinge.WebClient.Models
+{
+    public class InvitationViewModel
+    {
+        public Guid Guid { get; set; }
+
+        public string Invitee { get; set; }
+    }
+}
